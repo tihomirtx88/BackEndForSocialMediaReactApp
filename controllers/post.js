a206afc9786808977efc96d248b53a4bf4d651bf
Tihomir Zhelyazkov <88166066+tihomirtx88@users.noreply.github.com>
2022-11-14 15:53:51 +0200

    Create create post logic

--- controllers/post.js
+++ controllers/post.js
@@ -1,12 +1,13 @@
 import { db } from "../connect.js";
 import jwt from "jsonwebtoken";
+import moment from "moment";
 
 export const getPosts = (req, res) => {
     const userId = req.query.userId;
     const token = req.cookies.accessToken;
     if (!token) return res.status(401).json("Not logged in!");
   
-    jwt.verify(token, "secretkey", (err, userInfo) => {
+    jwt.verify(token, "secretKey", (err, userInfo) => {
       if (err) return res.status(403).json("Token is not valid!");
   
       const q =
@@ -27,5 +28,24 @@ export const getPosts = (req, res) => {
   };
 
   export const addPost = (req, res) => {
-    
-  }
+    const token = req.cookies.accessToken;
+    if(!token) return res.status(401).json("Not logged in");
+    jwt.verify(token, "secretkey", (err, userInfo => {
+        if(err) return res.status(403).json("Token is not valid");
+
+        const q = 
+        "INSERT INTO posts(`description`, `image`, `createdAt`, `userId`) VALUES (?)";
+        const values = [
+            req.body.description,
+            req.body.image,
+            moment(Date.now()).format("YYYY-MM-DD HH:mm:ss"),
+            userInfo.id,
+        ];
+
+        db.query(1, [values], (err, data) => {
+            if (err) return res.status(500).json(err);
+            return res.status(200).json("Post are created")
+        });
+    }));
+
+  };
