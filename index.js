7f81a215b4f3461ab0112eea86256402669fd946
Tihomir Zhelyazkov <88166066+tihomirtx88@users.noreply.github.com>
2022-11-14 14:12:57 +0200

    Finish authentication and create on server side getPost logic

--- index.js
+++ index.js
@@ -11,15 +11,15 @@ import cookieParser from "cookie-parser";
 
 
 //middlewares
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Credentials", true);
+  next();
+});
 app.use(
   cors({
     origin: "http://localhost:3000",
   })
 );
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Credentials", true);
-  next();
-});
 app.use(express.json());
 app.use(cookieParser());
 
